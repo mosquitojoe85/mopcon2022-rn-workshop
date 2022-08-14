0628da47649d6060a5832b4c63232dad484aae34
brook <mosquitojoe85@gmail.com>
2022-08-14 17:07:00 +0800

    connect ropsten

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
+import "./global";
 import { useState, useEffect } from "react";
-import { View, ActivityIndicator, Text } from "react-native";
+import { View, ActivityIndicator } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import SendScreen from "./screens/SendScreen";
--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -7,11 +7,19 @@ import {
   ActivityIndicator,
   Modal,
 } from "react-native";
-import { Button, Card, List } from "react-native-paper";
+import { Button, Card, List, Appbar } from "react-native-paper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import Web3 from "web3";
 
-const HomeScreen = () => {
+const web3 = new Web3();
+web3.setProvider(
+  new web3.providers.HttpProvider(
+    "https://ropsten.infura.io/v3/dea49333d33447559dbd2a21ef3f6cc2"
+  )
+);
+
+const HomeScreen = (props) => {
   const [scanned, setScanned] = useState(false);
   const [state, setState] = useState({
     address: "",
@@ -21,8 +29,29 @@ const HomeScreen = () => {
     ETHBalance: null,
   });
 
+  const { navigation } = props;
+
+  const getETHBalance = async (address) => {
+    const balance = await web3.eth.getBalance(address);
+    const nextBalance = web3.utils.fromWei(balance, "ether");
+    return Number(nextBalance).toFixed(3);
+  };
+
   const checkWallet = async () => {
-    console.log("check Wallet status");
+    const accountStr = await AsyncStorage.getItem("account");
+    console.log(`accountStr--->`, accountStr);
+    if (accountStr) {
+      const account = JSON.parse(accountStr);
+      const balance = await getETHBalance(account.address);
+
+      setState({
+        ...account,
+        ETHBalance: balance,
+        isLoading: false,
+      });
+    } else {
+      navigation.replace("Welcome");
+    }
   };
 
   const requestPermissionsAsync = async () => {
@@ -44,11 +73,7 @@ const HomeScreen = () => {
 
       {state.ETHBalance ? (
         <View style={{ flexDirection: "row", marginBottom: 20 }}>
-          <Text
-            style={styles.balanceTextStyle}
-          >
-            {state.ETHBalance} ETH
-          </Text>
+          <Text style={styles.balanceTextStyle}>{state.ETHBalance} ETH</Text>
         </View>
       ) : (
         <ActivityIndicator size="large" color="#fff" />
@@ -58,12 +83,24 @@ const HomeScreen = () => {
         <Card.Actions>
           <View style={styles.rowStyle}>
             <View style={styles.columnStyle}>
-              <Button icon="send" mode="outlined" onPress={() => false}>
+              <Button
+                icon="send"
+                mode="outlined"
+                onPress={() => {
+                  navigation.navigate("SendETH");
+                }}
+              >
                 Send
               </Button>
             </View>
             <View style={styles.columnStyle}>
-              <Button icon="qrcode" mode="contained" onPress={() => false}>
+              <Button
+                icon="qrcode"
+                mode="contained"
+                onPress={() => {
+                  navigation.navigate("Qrcode");
+                }}
+              >
                 Receive
               </Button>
             </View>
@@ -80,9 +117,13 @@ const HomeScreen = () => {
         </Card.Actions>
       </Card>
       <Modal visible={scanned}>
+        <Appbar.Header>
+          <Appbar.BackAction onPress={() => setScanned(false)} />
+          <Appbar.Content title="Title" />
+        </Appbar.Header>
         <BarCodeScanner
+          style={{ flex: 1 }}
           onBarCodeScanned={!scanned ? undefined : handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
         />
       </Modal>
 
@@ -107,6 +148,7 @@ const HomeScreen = () => {
           mode="outlined"
           onPress={async () => {
             await AsyncStorage.removeItem("account");
+            navigation.replace("Welcome");
           }}
         >
           Logout
