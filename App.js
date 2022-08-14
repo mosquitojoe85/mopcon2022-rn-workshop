fa2419d48784517b5a921ac5461915d173e48854
brook <mosquitojoe85@gmail.com>
2022-08-14 13:45:45 +0800

    add custom hook

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState, useEffect } from "react";
+
+const useCountdown = (defaultCount = 0, ms = 1000) => {
+  const [count, setCount] = useState(defaultCount);
+
+  useEffect(() => {
+    const timmer = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, ms);
+    return () => clearInterval(timmer);
+  }, []);
+  return [count, setCount];
+};
 
 function HomeScreen({ navigation }) {
+  const [count] = useCountdown(0, 1000);
+  const [anotherCount] = useCountdown(0, 500);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Text>You countdown {count} times</Text>
+      <Text>You countdown {anotherCount} times</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
